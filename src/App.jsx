7be974df9ba89b22e43e4f3c54b633cd16ac3315
Burhan Jawed <71589999+tibsxi@users.noreply.github.com>
2022-05-16 17:13:06 -0400

    added global styles to index.scss

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className='container'>
+      <h1 className='app__title'>Movie Search</h1>
+    </div>
+  );
+}
+
+export default App;
